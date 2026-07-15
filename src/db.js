5936eb48fb226ceedf0f9801603a4d7646fe0c6d
Youssef Nabil <146959536+youssefnabil2030@users.noreply.github.com>
2026-07-15 05:49:51 -0700

    Update src

---
+++ src/db.js
@@ -0,0 +1,12 @@
+// src/db.js
+import pg from 'pg';
+const { Pool } = pg;
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false 
+    }
+});
+
+export default pool;
