a89d57d874df645c21bd23c787e98101e9360bc1
Youssef Nabil <146959536+youssefnabil2030@users.noreply.github.com>
2026-07-19 18:08:08 -0700

    Create organizations.js

---
+++ src/models/organizations.js
@@ -0,0 +1,16 @@
+const db = require("./db")
+
+const Organization = {}
+
+Organization.getAll = async function() {
+  try {
+    const sql = "SELECT * FROM public.organizations ORDER BY name ASC"
+    const result = await db.query(sql)
+    return result.rows
+  } catch (error) {
+    console.error("Error inside Organization.getAll model: " + error)
+    throw error
+  }
+}
+
+module.exports = Organization
