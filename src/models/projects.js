434247b914f33c1e6cc96891e9ac7284319566f5
Youssef Nabil <146959536+youssefnabil2030@users.noreply.github.com>
2026-07-19 18:09:22 -0700

    Create projects.js

---
+++ src/models/projects.js
@@ -0,0 +1,16 @@
+const db = require("./db")
+
+const Project = {}
+
+Project.getAll = async function() {
+  try {
+    const sql = "SELECT * FROM public.projects ORDER BY name ASC"
+    const result = await db.query(sql)
+    return result.rows
+  } catch (error) {
+    console.error("Error inside Project.getAll model: " + error)
+    throw error
+  }
+}
+
+module.exports = Project
