db082fa10ca6a5d57ed367a187ffa9d74ec7d8c1
Youssef Nabil <146959536+youssefnabil2030@users.noreply.github.com>
2026-07-19 18:23:30 -0700

    Refactor Organization model to use ES6 imports

--- src/models/organizations.js
+++ src/models/organizations.js
@@ -1,16 +1,16 @@
-const db = require("./db.js")
+import db from '../db.js'; // Go up one directory level
 
-const Organization = {}
+const Organization = {};
 
 Organization.getAll = async function() {
   try {
-    const sql = "SELECT * FROM public.organizations ORDER BY name ASC"
-    const result = await db.query(sql)
-    return result.rows
+    const sql = "SELECT * FROM public.organizations ORDER BY name ASC";
+    const result = await db.query(sql);
+    return result.rows;
   } catch (error) {
-    console.error("Error inside Organization.getAll model: " + error)
-    throw error
+    console.error("Error inside Organization.getAll model: ", error);
+    throw error;
   }
-}
+};
 
-module.exports = Organization
+export default Organization;
