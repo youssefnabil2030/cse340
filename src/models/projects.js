1f24393d77876166abdbe181fd45b083de349c7f
Youssef Nabil <146959536+youssefnabil2030@users.noreply.github.com>
2026-07-19 18:23:57 -0700

    Refactor Project model to use ES6 import/export

--- src/models/projects.js
+++ src/models/projects.js
@@ -1,16 +1,16 @@
-const db = require("./db.js")
+import db from '../db.js'; // Go up one directory level
 
-const Project = {}
+const Project = {};
 
 Project.getAll = async function() {
   try {
-    const sql = "SELECT * FROM public.projects ORDER BY name ASC"
-    const result = await db.query(sql)
-    return result.rows
+    const sql = "SELECT * FROM public.projects ORDER BY name ASC";
+    const result = await db.query(sql);
+    return result.rows;
   } catch (error) {
-    console.error("Error inside Project.getAll model: " + error)
-    throw error
+    console.error("Error inside Project.getAll model: ", error);
+    throw error;
   }
-}
+};
 
-module.exports = Project
+export default Project;
